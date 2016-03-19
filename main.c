4f761a145bdb148ddb83df42cc1e2c64a87530a7
Antonio Carlos do Nascimento Junior <jr_acn@yahoo.com.br>
2016-03-18 21:59:22 -0300

    Adicionando explicações aos métodos, criado método para remover ultimo elemento. criado método para imprimir maior número inserido.

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 #include <locale.h>
  
  
- /*
- 	Fazemos um sumário dos métodos para que possa ser compilado o código.
+/*
+ 	Fazemos um sumário dos métodos para que possa ser compilado sem erros pelo Dev-c++ o código.
 */
 float calcularMedia();
 void menuPrincipal();
@@ -13,6 +13,8 @@ int removerElemento();
 void imprimir();
 int removerElemento();
 void inserirUltimoElemento();
+int maiorNumeroVetor();
+int removerUltimoElemento();
 
 /*
 	Struct NO
@@ -20,13 +22,20 @@ void inserirUltimoElemento();
 	o atributo [prox] na verdade sempre será o [valor] anterior.
 	
 	Exemplo de input de dados [1,2,3,4,5,6]
-		será processado como  [6,5,4,3,2,1]
+		será processado como  [6,5,4,3,2,1] 
+		Pois é uma pilha, ou melhor uma lista encadeada.
 */
+
 struct no{
 	int info;
 	struct no * prox;
 };
 
+/*
+ 	Método Main, será responsável por administrar o valor
+ 	input do menu principal, e distribuir seus valores
+ 	a suas devidas ações.
+*/
 int main(int argc, char *argv[]) {
 
 	setlocale(LC_ALL,"portuguese");
@@ -66,14 +75,20 @@ int main(int argc, char *argv[]) {
 			   int valor = removerElemento(&inicio);
 			   
 			   if(valor != -1){
-			   	printf("%d sera o novo número",valor);
+			   	printf("%d é o numero removido",valor);
+			   	getch();
 			   }
 			    
 				break;
 			}
 			case 6:{
 			   int valor = removerUltimoElemento(&inicio);
-		 		    
+		 	   if(valor != -1){
+			   	printf("%d é o numero removido",valor);
+			   	getch();
+			   }
+			
+			
 				break;
 			}
 			case 7:{
@@ -86,11 +101,25 @@ int main(int argc, char *argv[]) {
 				getch();
 				break;
 			}
+			case 10:{
+		    int valor = maiorNumeroVetor(&inicio);
+		      if(valor != -1){
+			   	printf("%d é o maior número",valor);
+			   		getch();
+			   }
+		
+			break;
+			}
+			
 		}
 	}while(tecla != 9);
    
 }
 
+/*
+ 	Menu principal é responsável por printar
+ 	na tela, todas as opções disponíveis
+*/
 void menuPrincipal(){
 	system("cls");
 	printf("--- MALLOC ---\n\n");
@@ -102,9 +131,65 @@ void menuPrincipal(){
 	printf("6) Remover Último Elemento\n");
 	printf("7) Adicionar no final\n");
 	printf("8) Procurar número\n");
+	printf("10) Maior número\n");
 	printf("\n9) Sair\n\n");
 }
 
+/*
+	Inserir elemento
+	Podemos simular o seguinte cenário
+	1) Nenhum valor foi armazenado ainda, sendo assim de forma simples para
+		a explicação, pegaremos a instancia do struct NULL, instanciamos uma nova
+		struct p sem valores em [p=malloc(sizeof(struct no))]
+		
+		struct no p
+		{
+			info : 0,
+			prox : NULL
+		}
+	
+		apontamos o atributo -> info para a variável x ( Supomos que seja o valor 7)
+		e também atribuimos o atributo->prox = inicio (Lembrando que inicio é null)
+		
+		struct no p
+		{
+			info : x (7),
+			prox : inicio (NULL)
+		}
+		
+		no final apontamos inicio como p, ficando assim:
+		
+		struct no inicio
+		{
+			info : 7,
+			prox : inicio (NULL)
+		}	
+			
+	2) Nese cenário, já temos um valor apontando para inicio, sendo que sua struct estará como
+	
+		struct no inicio
+		{
+			info : 7,
+			prox : NULL
+		}
+		assim iremos gerar uma instancia, iremos atribuir o valor x que será (3) para info,
+		e para prox, apontamos o Inicio, sendo assim ele irá ficar assim:
+	
+		struct no p
+		{
+			info : x (3),
+			prox : 	struct no Inicio
+					{
+						info : x (7),
+						prox : inicio (NULL)
+					}
+		}
+		
+		Toda vez chamando o método, teremos uma lista encadeada infinita, desde que tenhamos
+		memória disponível no sistema.
+		
+		
+*/
 void inserirElemento(struct no **inicio){
 	
 	int x = 0;
@@ -125,6 +210,41 @@ void inserirElemento(struct no **inicio){
 		printf("Elemento inserido!\n");
  
 }
+void inserirUltimoElemento(struct no **inicio){
+	
+	int x = 0;
+	printf("Entre com um elemento: ");
+	scanf("%d", &x);
+	
+	struct no *p, *q;
+   
+  	if((p=malloc(sizeof(struct no))) == NULL){
+  		printf("Falha na alocação da memória\n\n");
+  		getch();
+	  }
+	  else{
+	  	p->info = x;
+	  	p->prox = NULL;
+	  	
+	  	if(*inicio == NULL)
+	  		*inicio = p;
+	  	else{
+	  		q = *inicio;
+	  		
+			while(q->prox != NULL)
+				q = q->prox;
+				
+			q->prox = p;
+				
+		 }
+	  }
+
+ 	*inicio = q; 
+	printf("Elemento %d inserido!\n",x);
+	getch();
+ 
+}
+
 void imprimir(struct no *inicio){
  struct no *p;
  
@@ -182,8 +302,9 @@ float calcularMedia(struct no *inicio){
 
 int removerElemento(struct no **inicio){
 
-	if(inicio == NULL){
+	if(*inicio == NULL){
  		printf("A Lista está vazia.");
+ 		getch();
  		return -1;
 	 }
 	 	
@@ -201,60 +322,37 @@ int removerElemento(struct no **inicio){
 
 int removerUltimoElemento(struct no **inicio){
 
-	if(inicio == NULL){
+	if(*inicio == NULL){
  		printf("A Lista está vazia.");
+ 		getch();
  		return -1;
 	 }
 	 	
-	struct no *p;
-	struct no *q;
- 	p = *inicio;
- 	q = *inicio;
- 	
- 	while(p != NULL){
- 			q = p;
- 			p = p->prox;
-	 }
- 	
-
- 	*inicio = q;
-   
- 	return 0;	
-}
-
-void inserirUltimoElemento(struct no **inicio){
+	struct no *p;	
+	struct no *q;		
+	p = *inicio;
+	q = *inicio;
 	
-	int x = 0;
-	printf("Entre com um elemento: ");
-	scanf("%d", &x);
+	if(p->prox == NULL){
+		int valor = p->info;
 	
-	struct no *p, *q;
-   
-  	if((p=malloc(sizeof(struct no))) == NULL){
-  		printf("Falha na alocação da memória\n\n");
-	  }
-	  else{
-	  	p->info = x;
-	  	p->prox = NULL;
-	  	
-	  	if(*inicio == NULL)
-	  		*inicio = p;
-	  	else{
-	  		q = *inicio;
-	  		
-			while(q->prox != NULL)
-				q = q->prox;
-				
-			q->prox = p;
-				
-		 }
-	  }
-
- 	*inicio = q; 
-	printf("Elemento %d inserido!\n",x);
+		*inicio = (*inicio)->prox; 
+		free (p);
+		p = NULL;
+		return valor;
+	}
+	
+	while(p->prox != NULL){
+		q = p;
+		p = p->prox;
+	}
+	
+	q->prox = NULL;
+	
+	inicio = q;
+	return p->info;
  
 }
- 
 
 void procurarExistencia (struct no ** inicio)
 {
@@ -274,6 +372,7 @@ void procurarExistencia (struct no ** inicio)
 	else{
 		printf("Foi encontrado \n");
 	}
+	getch();
    
 }
 
@@ -310,4 +409,18 @@ void iniciarMeio (struct no ** inicio)
 		}
 	}
    
+}
+int maiorNumeroVetor(struct no **inicio){
+	
+	int mostValue = 0;
+	struct no *p;
+	p = *inicio;
+	  while (p != NULL && p->info != mostValue) {
+	  	if(p->info > mostValue){
+	  		mostValue = p->info;
+		  }
+	  	p = p->prox; 
+	  }
+	  
+	  return mostValue;
 }
